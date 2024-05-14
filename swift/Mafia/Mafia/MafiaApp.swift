b353f2e9bb29fdcc56d4c8e21f09db58567ae902
Sanil Doshi <goatdev2001@gmail.com>
2024-05-13 19:34:43 -0700

    Added .gitignore for visionOS

---
+++ swift/Mafia/Mafia/MafiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaApp.swift
+//  Mafia
+//
+//  Created by Sanil Doshi on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MafiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
